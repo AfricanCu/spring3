9d95ff08caad7d5bc78872a90f4bf179c52bb473
zhoumingjun <africancu@126.com>
2018-04-11 23:46:54 +0800

    -Exception 异常回滚

--- spring_day03/src/main/java/cn/african/service/AccountServiceImpl.java
+++ spring_day03/src/main/java/cn/african/service/AccountServiceImpl.java
@@ -5,7 +5,6 @@ import cn.african.dao.AccountDao;
 public class AccountServiceImpl implements AccountService {
 
 	private AccountDao accountDao;
-
 	public void setAccountDao(AccountDao accountDao) {
 		this.accountDao = accountDao;
 	}
