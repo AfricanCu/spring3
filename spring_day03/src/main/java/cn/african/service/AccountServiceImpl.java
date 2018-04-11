235b03a65d53d9fabcf70922de3fd238e7555a30
zhoumingjun <africancu@126.com>
2018-04-11 23:44:00 +0800

    事务没有回滚

--- spring_day03/src/main/java/cn/african/service/AccountServiceImpl.java
+++ spring_day03/src/main/java/cn/african/service/AccountServiceImpl.java
@@ -1,9 +1,5 @@
 package cn.african.service;
 
-import org.springframework.transaction.TransactionStatus;
-import org.springframework.transaction.support.TransactionCallbackWithoutResult;
-import org.springframework.transaction.support.TransactionTemplate;
-
 import cn.african.dao.AccountDao;
 
 public class AccountServiceImpl implements AccountService {
@@ -14,21 +10,11 @@ public class AccountServiceImpl implements AccountService {
 		this.accountDao = accountDao;
 	}
 
-	private TransactionTemplate transactionTemplate;
-
-	public void setTransactionTemplate(TransactionTemplate transactionTemplate) {
-		this.transactionTemplate = transactionTemplate;
-	}
 
 	@Override
 	public void transfer(String outer, String inner, Integer money) {
-		transactionTemplate.execute(new TransactionCallbackWithoutResult() {
-			@Override
-			protected void doInTransactionWithoutResult(TransactionStatus arg0) {
-				accountDao.out(outer, money);
-				int i = 1 / 0;
-				accountDao.in(inner, money);
-			}
-		});
+		accountDao.out(outer, money);
+		int i = 1 / 0;
+		accountDao.in(inner, money);
 	}
 }
